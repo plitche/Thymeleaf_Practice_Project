5fc493ad24a0126f0c08dd5fc5a216fce901bb6b
plitche <kysu728@naver.com>
2022-11-06 16:51:52 +0900

    spring boot 동작 확인

---
+++ Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.Thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String getMain() {
+        return "main.html";
+    }
+
+}
