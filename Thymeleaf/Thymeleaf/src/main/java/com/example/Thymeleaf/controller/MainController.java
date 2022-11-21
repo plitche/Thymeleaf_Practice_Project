fc11c408e4835793fb019e42994dabd3dcb4f7c3
plitche <kysu728@naver.com>
2022-11-21 22:32:15 +0900

    로그인 기능 구현 개발 중

--- Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MainController.java
+++ Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MainController.java
@@ -19,11 +19,11 @@ public class MainController {
 
     @RequestMapping("/")
     public String getMain(@RequestParam(required = false) Boolean result,
-//                          MemberDto memberDto,
+                          MemberDto memberDto,
                           Model model) throws Exception {
         model.addAttribute("productList", productService.getProductList());
         model.addAttribute("result", result);
-//        model.addAttribute("memberDto", memberDto);
+        model.addAttribute("memberDto", memberDto);
         return "main";
     }
 
