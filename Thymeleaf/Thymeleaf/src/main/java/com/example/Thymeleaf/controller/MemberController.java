5afa312ac1144bdb2aba3feb99c7e2b3ba39b1be
plitche <kysu728@naver.com>
2022-11-24 22:36:13 +0900

    login session thymeleaf

--- Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MemberController.java
+++ Thymeleaf/Thymeleaf/src/main/java/com/example/Thymeleaf/controller/MemberController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
@@ -20,12 +25,27 @@ public class MemberController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public MemberDto login(@RequestParam String id,
-                      @RequestParam String password,
-                      Model model) throws Exception {
-        System.out.println("id = " + id);
-        System.out.println("password = " + password);
-        MemberDto memberDto = memberService.login(id, password);
-        return memberDto;
+    public Map<String, Object> login(@RequestParam String id,
+                     @RequestParam String password,
+                     HttpServletRequest request,
+                     Model model) throws Exception {
+//        System.out.println("id = " + id);
+//        System.out.println("password = " + password);
+
+        Map<String, Object> returnMap = new HashMap<>();
+        try {
+            MemberDto memberDto = memberService.login(id, password);
+
+            HttpSession httpSession = request.getSession();
+            httpSession.setAttribute("memberId", memberDto.getId());
+
+            returnMap.put("result", true);
+            returnMap.put("message", "로그인에 성공하셨습니다.");
+        } catch (Exception e) {
+            returnMap.put("result", false);
+            returnMap.put("message", "로그인에 실패하셨습니다. 다시 시도해주세요.");
+        }
+
+        return returnMap;
     }
 }
